98f81758bdf230eee44e9706d77692e20a107b8b
Maks <maxiomaxiowski8@gmail.com>
2024-08-09 19:50:37 +0100

    changed error printer to use memory stream

--- src/Result.c
+++ src/Result.c
@@ -3,30 +3,14 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
-
-#include "data-structures/Array.h"
-
-static void AddString(Array* charArray, const char* string)
-{
-	size_t i = 0;
-	while(1)
-	{
-		char current = string[i];
-
-		if (current == '\0')
-			break;
-
-		ArrayAdd(charArray, &current);
-
-		i++;
-	}
-}
+#include <string.h>
+#include <data-structures/MemoryStream.h>
 
 void PrintError(const Result result)
 {
 	assert(result.errorMessage != NULL);
 
-	Array output = AllocateArray(sizeof(char));
+	MemoryStream* stream = AllocateMemoryStream();
 
 	size_t i = 0;
 	while (1)
@@ -35,12 +19,14 @@ void PrintError(const Result result)
 			result.errorMessage[i + 1] == 't')
 		{
 			i += 2;
-			AddString(&output, GetTokenTypeString(result.tokenType));
+
+			const char* tokenName = GetTokenTypeString(result.tokenType);
+			StreamWrite(stream, tokenName, strlen(tokenName));
 		}
 
-		char current = result.errorMessage[i];
+		const char current = result.errorMessage[i];
 
-		ArrayAdd(&output, &current);
+		StreamWriteByte(stream, current);
 
 		if (current == '\0')
 			break;
@@ -48,12 +34,7 @@ void PrintError(const Result result)
 		i++;
 	}
 
-	char* buffer = malloc(output.length * sizeof(char));
-	// ReSharper disable once CppDeclarationHidesLocal
-	for (int i = 0; i < output.length; ++i)
-		buffer[i] = *((char*)output.array[i]);
-	FreeArray(&output);
-
+	char* buffer = (char*)FreeMemoryStream(stream, false).buffer;
 	printf("%s (line %"PRIu32")\n", buffer, result.lineNumber);\
 	free(buffer);
 }
