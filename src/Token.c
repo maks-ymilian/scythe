f20607c5364759327f27de4a4c3e96bbcad4cb48
Maks <maxiomaxiowski8@gmail.com>
2024-10-17 21:49:12 +0100

    formatting

--- src/Token.c
+++ src/Token.c
@@ -9,67 +9,67 @@ const char* GetTokenTypeString(const TokenType tokenType)
 {
     switch (tokenType)
     {
-        case LeftBracket: return "(";
-        case RightBracket: return ")";
-        case LeftCurlyBracket: return "{";
-        case RightCurlyBracket: return "}";
-        case LeftSquareBracket: return "[";
-        case RightSquareBracket: return "]";
-        case Dot: return ".";
-        case Comma: return ",";
-        case Semicolon: return ";";
-        case Plus: return "+";
-        case Minus: return "-";
-        case Asterisk: return "*";
-        case Slash: return "/";
-        case Exclamation: return "!";
-        case Equals: return "=";
-        case At: return "@";
-        case LeftAngleBracket: return "<";
-        case RightAngleBracket: return ">";
-        case Ampersand: return "&";
-        case Pipe: return "|";
+    case LeftBracket: return "(";
+    case RightBracket: return ")";
+    case LeftCurlyBracket: return "{";
+    case RightCurlyBracket: return "}";
+    case LeftSquareBracket: return "[";
+    case RightSquareBracket: return "]";
+    case Dot: return ".";
+    case Comma: return ",";
+    case Semicolon: return ";";
+    case Plus: return "+";
+    case Minus: return "-";
+    case Asterisk: return "*";
+    case Slash: return "/";
+    case Exclamation: return "!";
+    case Equals: return "=";
+    case At: return "@";
+    case LeftAngleBracket: return "<";
+    case RightAngleBracket: return ">";
+    case Ampersand: return "&";
+    case Pipe: return "|";
 
-        case EqualsEquals: return "==";
-        case ExclamationEquals: return "!=";
-        case LeftAngleEquals: return "<=";
-        case RightAngleEquals: return ">=";
-        case AmpersandAmpersand: return "&&";
-        case PipePipe: return "||";
-        case PlusPlus: return "++";
-        case MinusMinus: return "--";
-        case PlusEquals: return "+=";
-        case MinusEquals: return "-=";
-        case AsteriskEquals: return "*=";
-        case SlashEquals: return "/=";
+    case EqualsEquals: return "==";
+    case ExclamationEquals: return "!=";
+    case LeftAngleEquals: return "<=";
+    case RightAngleEquals: return ">=";
+    case AmpersandAmpersand: return "&&";
+    case PipePipe: return "||";
+    case PlusPlus: return "++";
+    case MinusMinus: return "--";
+    case PlusEquals: return "+=";
+    case MinusEquals: return "-=";
+    case AsteriskEquals: return "*=";
+    case SlashEquals: return "/=";
 
-        case NumberLiteral: return "NumberLiteral";
-        case StringLiteral: return "StringLiteral";
-        case Identifier: return "Identifier";
-        case True: return "true";
-        case False: return "false";
+    case NumberLiteral: return "NumberLiteral";
+    case StringLiteral: return "StringLiteral";
+    case Identifier: return "Identifier";
+    case True: return "true";
+    case False: return "false";
 
-        case Init: return "init";
-        case Sample: return "sample";
-        case Serialize: return "serialize";
-        case Block: return "block";
-        case GFX: return "gfx";
-        case Slider: return "slider";
+    case Init: return "init";
+    case Sample: return "sample";
+    case Serialize: return "serialize";
+    case Block: return "block";
+    case GFX: return "gfx";
+    case Slider: return "slider";
 
-        case If: return "if";
-        case Else: return "else";
-        case Struct: return "struct";
-        case Return: return "return";
+    case If: return "if";
+    case Else: return "else";
+    case Struct: return "struct";
+    case Return: return "return";
 
-        case Void: return "void";
-        case Int: return "int";
-        case Float: return "float";
-        case String: return "string";
-        case Bool: return "bool";
+    case Void: return "void";
+    case Int: return "int";
+    case Float: return "float";
+    case String: return "string";
+    case Bool: return "bool";
 
-        case EndOfFile: return "EndOfFile";
+    case EndOfFile: return "EndOfFile";
 
-        default: assert(0);
+    default: assert(0);
     }
 }
 
