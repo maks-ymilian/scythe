e94d4c192076a278f87c89e5959398c130516f05
Maks <maxiomaxiowski8@gmail.com>
2025-01-16 01:48:44 +0000

    add default variable initializers

--- src/code-generation/CodeGenerator.c
+++ src/code-generation/CodeGenerator.c
@@ -1,14 +1,16 @@
 #include "CodeGenerator.h"
 
+#include "Writer.h"
+#include "passes/DefaultValuePass.h"
 #include "passes/GlobalSectionPass.h"
 #include "passes/ResolverPass.h"
 #include "passes/TypeConversionPass.h"
-#include "Writer.h"
 
 Result GenerateCode(const AST* syntaxTree, char** outputCode, size_t* outputLength)
 {
 	PROPAGATE_ERROR(ResolverPass(syntaxTree));
 	GlobalSectionPass(syntaxTree);
+	PROPAGATE_ERROR(DefaultValuePass(syntaxTree));
 	PROPAGATE_ERROR(TypeConversionPass(syntaxTree));
 	WriteOutput(syntaxTree, outputCode, outputLength);
 	return SUCCESS_RESULT;
--- src/code-generation/passes/DefaultValuePass.c
+++ src/code-generation/passes/DefaultValuePass.c
@@ -4,10 +4,72 @@
 
 static const char* currentFilePath = NULL;
 
+static PrimitiveType GetType(const NodePtr type)
+{
+	assert(type.type == Node_Literal);
+	const LiteralExpr* literal = type.ptr;
+	assert(literal->type == Literal_PrimitiveType);
+	return literal->primitiveType;
+}
+
+static Result VisitVariableDeclaration(VarDeclStmt* varDecl)
+{
+	if (varDecl->array)
+		return SUCCESS_RESULT;
+
+	if (varDecl->initializer.ptr != NULL)
+		return SUCCESS_RESULT;
+
+	switch (GetType(varDecl->type))
+	{
+	case Primitive_Void:
+		return ERROR_RESULT("\"void\" is not allowed here", varDecl->lineNumber, currentFilePath);
+
+	case Primitive_Float:
+	case Primitive_Int:
+		varDecl->initializer = AllocASTNode(
+			&(LiteralExpr){
+				.lineNumber = varDecl->lineNumber,
+				.type = Literal_Int,
+				.intValue = 0,
+			},
+			sizeof(LiteralExpr), Node_Literal);
+		break;
+
+	case Primitive_Bool:
+		varDecl->initializer = AllocASTNode(
+			&(LiteralExpr){
+				.lineNumber = varDecl->lineNumber,
+				.type = Literal_Boolean,
+				.boolean = false,
+			},
+			sizeof(LiteralExpr), Node_Literal);
+		break;
+
+	case Primitive_String:
+		return ERROR_RESULT("Variables of type \"string\" must have an initializer", varDecl->lineNumber, currentFilePath);
+
+	default: unreachable();
+	}
+
+	return SUCCESS_RESULT;
+}
+
 static Result VisitStatement(const NodePtr* node)
 {
 	switch (node->type)
 	{
+	case Node_VariableDeclaration:
+		PROPAGATE_ERROR(VisitVariableDeclaration(node->ptr));
+		break;
+	case Node_Block:
+		const BlockStmt* block = node->ptr;
+		for (size_t i = 0; i < block->statements.length; ++i)
+			PROPAGATE_ERROR(VisitStatement(block->statements.array[i]));
+		break;
+	case Node_ExpressionStatement:
+	case Node_If:
+		break;
 	default: unreachable();
 	}
 	return SUCCESS_RESULT;
