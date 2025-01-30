1f04dbc0801a83867db3f975c3b1d0bcfe62c8be
Maks <maxiomaxiowski8@gmail.com>
2025-01-30 15:21:47 +0000

    expand function call arguments

--- src/code-generation/passes/MemberExpansionPass.c
+++ src/code-generation/passes/MemberExpansionPass.c
@@ -64,13 +64,15 @@ static VarDeclStmt* GetVarDeclFromMemberAccessValue(const NodePtr value)
 	}
 }
 
-static IdentifierReference* GetIdentifier(const NodePtr memberAccessNode)
+static IdentifierReference* GetIdentifier(const MemberAccessExpr* memberAccess)
 {
-	assert(memberAccessNode.type == Node_MemberAccess);
-	const MemberAccessExpr* memberAccess = memberAccessNode.ptr;
-	assert(memberAccess->value.type == Node_Literal);
+	if (memberAccess->value.type != Node_Literal)
+		return NULL;
+
 	LiteralExpr* literal = memberAccess->value.ptr;
-	assert(literal->type == Literal_Identifier);
+	if (literal->type != Literal_Identifier)
+		return NULL;
+
 	return &literal->identifier;
 }
 
@@ -82,9 +84,12 @@ static void UpdateStructMemberAccess(const NodePtr memberAccessNode)
 	if (varDecl == NULL || GetStructDecl(varDecl->type) == NULL)
 		return;
 
-	MemberAccessExpr* next = memberAccess;
+	const MemberAccessExpr* next = memberAccess;
 	while (true)
 	{
+		if (next->next.ptr == NULL)
+			return;
+
 		assert(next->next.type == Node_MemberAccess);
 		next = next->next.ptr;
 
@@ -94,8 +99,10 @@ static void UpdateStructMemberAccess(const NodePtr memberAccessNode)
 			break;
 	}
 
-	IdentifierReference* currentIdentifier = GetIdentifier(memberAccessNode);
-	const IdentifierReference* nextIdentifier = GetIdentifier((NodePtr){.ptr = next, .type = Node_MemberAccess});
+	IdentifierReference* currentIdentifier = GetIdentifier(memberAccess);
+	const IdentifierReference* nextIdentifier = GetIdentifier(next);
+	assert(currentIdentifier != NULL);
+	assert(nextIdentifier != NULL);
 
 	VarDeclStmt* instantiated = FindInstantiated(nextIdentifier->text, varDecl);
 	assert(instantiated != NULL);
@@ -139,6 +146,98 @@ static void RemoveModuleAccess(NodePtr* memberAccessNode)
 	*memberAccessNode = next;
 }
 
+static StructDeclStmt* GetStructVariableFromMemberAccess(const MemberAccessExpr* memberAccess)
+{
+	while (memberAccess->next.ptr != NULL)
+	{
+		assert(memberAccess->next.type == Node_MemberAccess);
+		memberAccess = memberAccess->next.ptr;
+	}
+
+	const VarDeclStmt* varDecl = GetVarDeclFromMemberAccessValue(memberAccess->value);
+	if (varDecl == NULL)
+		return NULL;
+
+	StructDeclStmt* structDecl = GetStructDecl(varDecl->type);
+	if (structDecl == NULL)
+		return NULL;
+
+	return structDecl;
+}
+
+static void AddToEnd(const NodePtr startMemberAccessNode, const NodePtr endMemberAccessNode)
+{
+	assert(startMemberAccessNode.type == Node_MemberAccess);
+	assert(endMemberAccessNode.type == Node_MemberAccess);
+	MemberAccessExpr* end = startMemberAccessNode.ptr;
+	while (end->next.ptr != NULL)
+	{
+		assert(end->next.type == Node_MemberAccess);
+		end = end->next.ptr;
+	}
+	end->next = endMemberAccessNode;
+}
+
+static void VisitExpression(NodePtr* node);
+
+static void ExpandFunctionCallArguments(const NodePtr* memberAccessNode)
+{
+	assert(memberAccessNode->type == Node_MemberAccess);
+	const MemberAccessExpr* memberAccess = memberAccessNode->ptr;
+	if (memberAccess->value.type != Node_FunctionCall)
+		return;
+
+	FuncCallExpr* funcCall = memberAccess->value.ptr;
+	for (size_t i = 0; i < funcCall->arguments.length; ++i)
+	{
+		const NodePtr argument = *(NodePtr*)funcCall->arguments.array[i];
+		if (argument.type != Node_MemberAccess)
+			continue;
+
+		const MemberAccessExpr* memberAccess = argument.ptr;
+		const StructDeclStmt* structDecl = GetStructVariableFromMemberAccess(memberAccess);
+		if (structDecl == NULL)
+			continue;
+
+		ArrayRemove(&funcCall->arguments, i);
+		for (size_t j = 0; j < structDecl->members.length; ++j)
+		{
+			NodePtr copy = CopyASTNode(argument);
+			ArrayInsert(&funcCall->arguments, &copy, i);
+
+			const NodePtr* memberNode = structDecl->members.array[j];
+			assert(memberNode->type == Node_VariableDeclaration);
+			VarDeclStmt* member = memberNode->ptr;
+
+			const NodePtr end = AllocASTNode(
+				&(MemberAccessExpr){
+					.next = NULL_NODE,
+					.value = AllocASTNode(
+						&(LiteralExpr){
+							.lineNumber = -1,
+							.type = Literal_Identifier,
+							.identifier = (IdentifierReference){
+								.text = AllocateString(member->name),
+								.reference = (NodePtr){.ptr = member, .type = Node_VariableDeclaration},
+							},
+						},
+						sizeof(LiteralExpr), Node_Literal),
+				},
+				sizeof(MemberAccessExpr), Node_MemberAccess);
+			AddToEnd(copy, end);
+		}
+		i += structDecl->members.length;
+
+		FreeASTNode(argument);
+	}
+
+	for (size_t i = 0; i < funcCall->arguments.length; ++i)
+	{
+		NodePtr* node = funcCall->arguments.array[i];
+		VisitExpression(node);
+	}
+}
+
 static void VisitExpression(NodePtr* node)
 {
 	switch (node->type)
@@ -146,6 +245,7 @@ static void VisitExpression(NodePtr* node)
 	case Node_MemberAccess:
 		RemoveModuleAccess(node);
 		UpdateStructMemberAccess(*node);
+		ExpandFunctionCallArguments(node);
 
 		MemberAccessExpr* memberAccess = node->ptr;
 		assert(memberAccess->next.ptr == NULL);
