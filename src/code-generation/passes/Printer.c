50fb17a9866cae862953728905081d66ae94f651
maks <>
2025-06-17 20:37:53 +0100

    update printer

--- src/code-generation/passes/Printer.c
+++ src/code-generation/passes/Printer.c
@@ -1,174 +1,326 @@
 #include "Printer.h"
 
+#include <string.h>
+#include <stdarg.h>
+
+static int indentationLevel;
+
+static void PrintIndentation(void)
+{
+	printf("\r");
+	for (int i = 0; i < indentationLevel; ++i)
+		printf("|\t");
+}
+
+static void Print(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	vprintf(format, args);
+
+	if (format[strlen(format) - 1] == '\n')
+		PrintIndentation();
+
+	va_end(args);
+}
+
+static void PushIndent(void)
+{
+	++indentationLevel;
+	PrintIndentation();
+}
+
+static void PopIndent(void)
+{
+	--indentationLevel;
+	PrintIndentation();
+}
+
 static void VisitStatement(const NodePtr* node);
 static void VisitExpression(const NodePtr* node)
 {
-	printf("(");
 	switch (node->type)
 	{
 	case Node_Binary:
 	{
-		printf("Binary");
+		Print("Binary\n");
+		PushIndent();
+
+
 		const BinaryExpr* binary = node->ptr;
+		Print("operatorType: %d\n", binary->operatorType);
+
+		Print("left: ");
 		VisitExpression(&binary->left);
+		Print("right: ");
 		VisitExpression(&binary->right);
+
+		PopIndent();
 		break;
 	}
 	case Node_Unary:
 	{
-		printf("Unary");
+		Print("Unary\n");
+
+		PushIndent();
 		const UnaryExpr* unary = node->ptr;
 		VisitExpression(&unary->expression);
+
+		PopIndent();
 		break;
 	}
 	case Node_MemberAccess:
 	{
+		Print("MemberAccess\n");
+		PushIndent();
+
 		const MemberAccessExpr* memberAccess = node->ptr;
-		printf("MemberAccess %d ", memberAccess->lineNumber);
-		VisitExpression(&memberAccess->start);
+
+		Print("identifiers: ");
 		for (size_t i = 0; i < memberAccess->identifiers.length; ++i)
-			printf(".%s", *(char**)memberAccess->identifiers.array[i]);
+			Print("%s ", *(char**)memberAccess->identifiers.array[i]);
+		Print("\n");
+
+		Print("start:\n");
+		PushIndent();
+		VisitExpression(&memberAccess->start);
+		PopIndent();
+
+		PopIndent();
 		break;
 	}
 	case Node_Subscript:
 	{
-		printf("Subscript");
+		Print("Subscript\n");
+		PushIndent();
+
 		const SubscriptExpr* subscript = node->ptr;
 		VisitExpression(&subscript->baseExpr);
 		VisitExpression(&subscript->indexExpr);
+
+		PopIndent();
 		break;
 	}
 	case Node_FunctionCall:
 	{
+		Print("FunctionCall\n");
+		PushIndent();
+
 		const FuncCallExpr* funcCall = node->ptr;
-		printf("FunctionCall %d ", funcCall->lineNumber);
+		Print("base: ");
 		VisitExpression(&funcCall->baseExpr);
+
+		Print("arguments:\n");
+		PushIndent();
 		for (size_t i = 0; i < funcCall->arguments.length; ++i)
 			VisitExpression(funcCall->arguments.array[i]);
+		PopIndent();
+
+		PopIndent();
 		break;
 	}
 	case Node_BlockExpression:
 	{
-		printf("BlockExpression");
+		Print("BlockExpression\n");
+		PushIndent();
+
 		// const BlockExpr* blockExpr = node->ptr;
 		// VisitStatement(&blockExpr->block);
+
+		PopIndent();
 		break;
 	}
 	case Node_Literal:
 	{
+		Print("Literal\n");
+		PushIndent();
+
 		LiteralExpr* literal = node->ptr;
-		printf("Literal");
 		if (literal->type == Literal_Number)
-			printf(":%s", literal->number);
+			Print("%s\n", literal->number);
+
+		PopIndent();
 		break;
 	}
 	case Node_SizeOf:
 	{
-		printf("SizeOf");
+		Print("SizeOf\n");
 		break;
 	}
 	case Node_Null:
 	{
-		printf("Null");
+		Print("Null\n");
 		break;
 	}
 	default: INVALID_VALUE(node->type);
 	}
-	printf(")");
 }
 
 static void VisitStatement(const NodePtr* node)
 {
-	printf("Statement:\n");
 	switch (node->type)
 	{
 	case Node_BlockStatement:
 	{
+		Print("BlockStatement\n");
+		PushIndent();
+
 		const BlockStmt* block = node->ptr;
 		for (size_t i = 0; i < block->statements.length; ++i)
 			VisitStatement(block->statements.array[i]);
+
+		PopIndent();
 		break;
 	}
 	case Node_Section:
 	{
+		Print("Section\n");
+		PushIndent();
+
 		const SectionStmt* section = node->ptr;
 		VisitStatement(&section->block);
+
+		PopIndent();
 		break;
 	}
 	case Node_If:
 	{
+		Print("If\n");
+		PushIndent();
+
 		const IfStmt* ifStmt = node->ptr;
 		VisitStatement(&ifStmt->trueStmt);
 		VisitStatement(&ifStmt->falseStmt);
 		VisitExpression(&ifStmt->expr);
+
+		PopIndent();
 		break;
 	}
 	case Node_FunctionDeclaration:
 	{
+		Print("FuncDeclStmt\n");
+		PushIndent();
+
 		const FuncDeclStmt* funcDecl = node->ptr;
 		for (size_t i = 0; i < funcDecl->parameters.length; ++i)
 			VisitStatement(funcDecl->parameters.array[i]);
 		VisitStatement(&funcDecl->block);
+
+		PopIndent();
 		break;
 	}
 	case Node_Return:
 	{
+		Print("ReturnStmt\n");
+		PushIndent();
+
 		const ReturnStmt* returnStmt = node->ptr;
 		VisitExpression(&returnStmt->expr);
+
+		PopIndent();
 		break;
 	}
 	case Node_VariableDeclaration:
 	{
+		Print("VarDeclStmt\n");
+		PushIndent();
+
 		const VarDeclStmt* varDecl = node->ptr;
 		VisitExpression(&varDecl->initializer);
+
+		PopIndent();
 		break;
 	}
 	case Node_StructDeclaration:
 	{
+		Print("StructDeclStmt\n");
+		PushIndent();
+
 		const StructDeclStmt* structDecl = node->ptr;
 		for (size_t i = 0; i < structDecl->members.length; ++i)
 			VisitStatement(structDecl->members.array[i]);
+
+		PopIndent();
 		break;
 	}
 	case Node_ExpressionStatement:
 	{
+		Print("ExpressionStmt\n");
+		PushIndent();
+
 		const ExpressionStmt* exprStmt = node->ptr;
 		VisitExpression(&exprStmt->expr);
+
+		PopIndent();
 		break;
 	}
 	case Node_While:
 	{
+		Print("WhileStmt\n");
+		PushIndent();
+
 		const WhileStmt* whileStmt = node->ptr;
 		VisitExpression(&whileStmt->expr);
 		VisitStatement(&whileStmt->stmt);
+
+		PopIndent();
 		break;
 	}
 	case Node_For:
 	{
+		Print("ForStmt\n");
+		PushIndent();
+
 		const ForStmt* forStmt = node->ptr;
 		VisitStatement(&forStmt->initialization);
 		VisitExpression(&forStmt->condition);
 		VisitExpression(&forStmt->increment);
 		VisitStatement(&forStmt->stmt);
+
+		PopIndent();
 		break;
 	}
 	case Node_Import:
 	{
+		Print("ImportStmt\n");
+		PushIndent();
+
 		const ImportStmt* importStmt = node->ptr;
-		printf("path:%s\nmodule:%s\n", importStmt->path, importStmt->moduleName);
+		Print("path:%s\n", importStmt->path);
+		Print("module:%s\n", importStmt->moduleName);
+
+		PopIndent();
+		break;
 	}
 	case Node_Input:
-	case Node_Modifier:
-	case Node_LoopControl:
-	case Node_Null:
+	{
+		Print("InputStmt\n");
 		break;
+	}
+	case Node_Modifier:
+	{
+		Print("ModifierStmt\n");
+		break;
+	}
+	case Node_LoopControl:
+	{
+		Print("LoopControlStmt\n");
+		break;
+	}
+	case Node_Null:
+	{
+		Print("Null\n");
+		break;
+	}
 	default: INVALID_VALUE(node->type);
 	}
-	printf("\n\n");
 }
 
 void Printer(const AST* ast)
 {
+	indentationLevel = 0;
+
 	for (size_t i = 0; i < ast->nodes.length; ++i)
 	{
 		const NodePtr* node = ast->nodes.array[i];
@@ -176,8 +328,13 @@ void Printer(const AST* ast)
 		ASSERT(node->type == Node_Module);
 		const ModuleNode* module = node->ptr;
 
-		printf("*************************************** MODULE: %s *****************************************\n", module->moduleName);
+		Print("*************************************** MODULE: %s *****************************************\n", module->moduleName);
+		ASSERT(indentationLevel == 0);
+		PushIndent();
+
 		for (size_t i = 0; i < module->statements.length; ++i)
 			VisitStatement(module->statements.array[i]);
+
+		PopIndent();
 	}
 }
