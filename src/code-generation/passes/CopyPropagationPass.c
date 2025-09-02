58b420db0f1ba52d8f8d23c39217e726ed8d97a7
totally real name <totallyrealemail@example.com>
2025-09-02 20:40:01 +0100

    add constant propagation

--- src/code-generation/passes/CopyPropagationPass.c
+++ src/code-generation/passes/CopyPropagationPass.c
@@ -8,11 +8,11 @@ typedef Map CopyAssignments;
 
 static FuncDeclStmt* currentFunction;
 
-static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST);
+static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST, bool modifyMap);
 
 static CopyAssignments AllocCopyAssignments()
 {
-	return AllocateMap(sizeof(VarDeclStmt*));
+	return AllocateMap(sizeof(NodePtr));
 }
 
 static CopyAssignments FreeCopyAssignments(const CopyAssignments* map)
@@ -39,14 +39,14 @@ static void MergeCopyAssignments(CopyAssignments* map1, const CopyAssignments* m
 		bool exists = false;
 		for (MAP_ITERATE(j, map2))
 		{
-			if (*(VarDeclStmt**)i->value == *(VarDeclStmt**)j->value && strcmp(i->key, j->key) == 0)
+			if (((NodePtr*)i->value)->ptr == ((NodePtr*)j->value)->ptr && strcmp(i->key, j->key) == 0)
 			{
 				exists = true;
 				break;
 			}
 		}
 
-		if (exists == false)
+		if (!exists)
 			ArrayAdd(&deleteKeys, &i->key);
 	}
 	FreeCopyAssignments(map2);
@@ -56,17 +56,17 @@ static void MergeCopyAssignments(CopyAssignments* map1, const CopyAssignments* m
 	FreeArray(&deleteKeys);
 }
 
-static VarDeclStmt* GetCopyAssignmentValue(const CopyAssignments* map, const VarDeclStmt* key)
+static NodePtr GetCopyAssignmentValue(const CopyAssignments* map, const VarDeclStmt* key)
 {
 	char keyStr[64];
 	snprintf(keyStr, sizeof(keyStr), "%p", (void*)key);
-	VarDeclStmt** value = MapGet(map, keyStr);
+	NodePtr* value = MapGet(map, keyStr);
 	if (!value)
-		return NULL;
+		return NULL_NODE;
 	return *value;
 }
 
-static bool AddCopyAssignment(CopyAssignments* map, const VarDeclStmt* key, const VarDeclStmt* value)
+static bool AddCopyAssignment(CopyAssignments* map, const VarDeclStmt* key, NodePtr value)
 {
 	char keyStr[64];
 	snprintf(keyStr, sizeof(keyStr), "%p", (void*)key);
@@ -77,12 +77,11 @@ static void DeleteAllPairsWithVar(CopyAssignments* map, const VarDeclStmt* var)
 {
 	char keyStr[64];
 	snprintf(keyStr, sizeof(keyStr), "%p", (void*)var);
-	VarDeclStmt** value = MapGet(map, keyStr);
 
 	Array deleteKeys = AllocateArray(sizeof(char*));
 	for (MAP_ITERATE(i, map))
 	{
-		if (*(VarDeclStmt**)i->value == var || strcmp(i->key, keyStr) == 0)
+		if (((NodePtr*)i->value)->ptr == var || strcmp(i->key, keyStr) == 0)
 			ArrayAdd(&deleteKeys, &i->key);
 	}
 
@@ -91,7 +90,33 @@ static void DeleteAllPairsWithVar(CopyAssignments* map, const VarDeclStmt* var)
 	FreeArray(&deleteKeys);
 }
 
-static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST)
+static bool ExprIsConstant(NodePtr expr)
+{
+	switch (expr.type)
+	{
+	case Node_Literal:
+	case Node_SizeOf:
+		return true;
+	case Node_MemberAccess:
+	case Node_Subscript:
+	case Node_FunctionCall:
+	case Node_BlockExpression:
+		return false;
+	case Node_Binary:
+	{
+		BinaryExpr* binary = expr.ptr;
+		return ExprIsConstant(binary->left) && ExprIsConstant(binary->right);
+	}
+	case Node_Unary:
+	{
+		UnaryExpr* unary = expr.ptr;
+		return ExprIsConstant(unary->expression);
+	}
+	default: INVALID_VALUE(expr.type);
+	}
+}
+
+static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST, bool modifyMap)
 {
 	switch (node->type)
 	{
@@ -107,9 +132,18 @@ static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST)
 		if (!memberAccess->varReference)
 			break;
 
-		VarDeclStmt* value = GetCopyAssignmentValue(map, memberAccess->varReference);
-		if (value && !(value->functionParamOf && value->functionParamOf != currentFunction))
-			memberAccess->varReference = value;
+		NodePtr valueNode = GetCopyAssignmentValue(map, memberAccess->varReference);
+		if (valueNode.type == Node_VariableDeclaration)
+		{
+			VarDeclStmt* value = valueNode.ptr;
+			if (!value->functionParamOf || value->functionParamOf == currentFunction)
+				memberAccess->varReference = value;
+		}
+		else if (valueNode.ptr)
+		{
+			FreeASTNode(*node);
+			*node = CopyASTNode(valueNode);
+		}
 		break;
 	}
 	case Node_Binary:
@@ -117,25 +151,27 @@ static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST)
 		BinaryExpr* binary = node->ptr;
 		if (binary->operatorType == Binary_Assignment)
 			ASSERT(binary->left.type != Node_MemberAccess);
-		VisitExpression(&binary->left, map, modifyAST);
-		VisitExpression(&binary->right, map, modifyAST);
+		VisitExpression(&binary->left, map, modifyAST, modifyMap);
+		VisitExpression(&binary->right, map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_Unary:
 	{
 		UnaryExpr* unary = node->ptr;
-		VisitExpression(&unary->expression, map, modifyAST);
+		VisitExpression(&unary->expression, map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_FunctionCall:
 	{
 		FuncCallExpr* funcCall = node->ptr;
 		for (size_t i = 0; i < funcCall->arguments.length; ++i)
-			VisitExpression(funcCall->arguments.array[i], map, modifyAST);
+			VisitExpression(funcCall->arguments.array[i], map, modifyAST, modifyMap);
 
 		ASSERT(funcCall->baseExpr.type == Node_MemberAccess);
 		MemberAccessExpr* baseExpr = funcCall->baseExpr.ptr;
 		ASSERT(baseExpr->funcReference);
+
+		CopyAssignments funcMap = AllocCopyAssignments();
 
 		FuncDeclStmt* funcDecl = baseExpr->funcReference;
 		currentFunction = funcDecl;
@@ -145,23 +181,25 @@ static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST)
 			ASSERT(node->type == Node_VariableDeclaration);
 			VarDeclStmt* varDecl = node->ptr;
 			varDecl->functionParamOf = funcDecl;
-			VisitStatement(node, map, modifyAST);
+			VisitStatement(node, &funcMap, modifyAST, modifyMap);
 		}
-		VisitStatement(&funcDecl->block, map, modifyAST);
+		VisitStatement(&funcDecl->block, &funcMap, modifyAST, modifyMap);
 		currentFunction = NULL;
+
+		MergeCopyAssignments(map, &funcMap);
 		break;
 	}
 	case Node_Subscript:
 	{
 		SubscriptExpr* subscript = node->ptr;
-		VisitExpression(&subscript->baseExpr, map, modifyAST);
-		VisitExpression(&subscript->indexExpr, map, modifyAST);
+		VisitExpression(&subscript->baseExpr, map, modifyAST, modifyMap);
+		VisitExpression(&subscript->indexExpr, map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_BlockExpression:
 	{
 		BlockExpr* blockExpr = node->ptr;
-		VisitStatement(&blockExpr->block, map, modifyAST);
+		VisitStatement(&blockExpr->block, map, modifyAST, modifyMap);
 		break;
 	}
 	default: INVALID_VALUE(node->type);
@@ -171,19 +209,13 @@ static void VisitExpression(NodePtr* node, CopyAssignments* map, bool modifyAST)
 static void ProcessAssignment(NodePtr node, CopyAssignments* map)
 {
 	VarDeclStmt* assignmentLeft = NULL;
-	VarDeclStmt* assignmentRight = NULL;
+	NodePtr assignmentRight = NULL_NODE;
 
 	if (node.type == Node_VariableDeclaration)
 	{
 		VarDeclStmt* varDecl = node.ptr;
 		assignmentLeft = varDecl;
-
-		if (varDecl->initializer.type == Node_MemberAccess)
-		{
-			MemberAccessExpr* right = varDecl->initializer.ptr;
-			ASSERT(right->varReference);
-			assignmentRight = right->varReference;
-		}
+		assignmentRight = varDecl->initializer;
 	}
 	else if (node.type == Node_Binary)
 	{
@@ -194,13 +226,7 @@ static void ProcessAssignment(NodePtr node, CopyAssignments* map)
 		ASSERT(left->varReference);
 		assignmentLeft = left->varReference;
 
-		if (binary->left.ptr != binary->right.ptr &&
-			binary->right.type == Node_MemberAccess)
-		{
-			MemberAccessExpr* right = binary->right.ptr;
-			ASSERT(right->varReference);
-			assignmentRight = right->varReference;
-		}
+		assignmentRight = binary->right;
 	}
 	else
 		UNREACHABLE();
@@ -208,14 +234,21 @@ static void ProcessAssignment(NodePtr node, CopyAssignments* map)
 	ASSERT(assignmentLeft);
 	DeleteAllPairsWithVar(map, assignmentLeft);
 
-	if (assignmentRight)
+	if (assignmentRight.type == Node_MemberAccess && assignmentLeft != ((MemberAccessExpr*)assignmentRight.ptr)->varReference)
+	{
+		MemberAccessExpr* memberAccess = assignmentRight.ptr;
+		ASSERT(memberAccess->varReference);
+		bool success = AddCopyAssignment(map, assignmentLeft, (NodePtr){.ptr = memberAccess->varReference, .type = Node_VariableDeclaration});
+		ASSERT(success);
+	}
+	else if (assignmentRight.ptr && ExprIsConstant(assignmentRight))
 	{
 		bool success = AddCopyAssignment(map, assignmentLeft, assignmentRight);
 		ASSERT(success);
 	}
 }
 
-static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST)
+static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST, bool modifyMap)
 {
 	switch (node->type)
 	{
@@ -232,41 +265,43 @@ static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST)
 			((BinaryExpr*)exprStmt->expr.ptr)->left.type == Node_MemberAccess)
 		{
 			BinaryExpr* binary = exprStmt->expr.ptr;
-			VisitExpression(&binary->right, map, modifyAST);
-			ProcessAssignment((NodePtr){.ptr = binary, .type = Node_Binary}, map);
+			VisitExpression(&binary->right, map, modifyAST, modifyMap);
+			if (modifyMap)
+				ProcessAssignment((NodePtr){.ptr = binary, .type = Node_Binary}, map);
 		}
 		else
-			VisitExpression(&exprStmt->expr, map, modifyAST);
+			VisitExpression(&exprStmt->expr, map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_VariableDeclaration:
 	{
 		VarDeclStmt* varDecl = node->ptr;
-		VisitExpression(&varDecl->initializer, map, modifyAST);
-		ProcessAssignment(*node, map);
+		VisitExpression(&varDecl->initializer, map, modifyAST, modifyMap);
+		if (modifyMap)
+			ProcessAssignment(*node, map);
 		break;
 	}
 	case Node_Input:
 	{
 		InputStmt* input = node->ptr;
-		VisitStatement(&input->varDecl, map, modifyAST);
+		VisitStatement(&input->varDecl, map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_BlockStatement:
 	{
 		BlockStmt* block = node->ptr;
 		for (size_t i = 0; i < block->statements.length; ++i)
-			VisitStatement(block->statements.array[i], map, modifyAST);
+			VisitStatement(block->statements.array[i], map, modifyAST, modifyMap);
 		break;
 	}
 	case Node_If:
 	{
 		IfStmt* ifStmt = node->ptr;
-		VisitExpression(&ifStmt->expr, map, modifyAST);
+		VisitExpression(&ifStmt->expr, map, modifyAST, modifyMap);
 
 		CopyAssignments otherMap = CopyCopyAssignments(map);
-		VisitStatement(&ifStmt->falseStmt, &otherMap, modifyAST);
-		VisitStatement(&ifStmt->trueStmt, map, modifyAST);
+		VisitStatement(&ifStmt->falseStmt, &otherMap, modifyAST, modifyMap);
+		VisitStatement(&ifStmt->trueStmt, map, modifyAST, modifyMap);
 		MergeCopyAssignments(map, &otherMap);
 		break;
 	}
@@ -275,12 +310,13 @@ static void VisitStatement(NodePtr* node, CopyAssignments* map, bool modifyAST)
 		WhileStmt* whileStmt = node->ptr;
 		CopyAssignments beforeMap = CopyCopyAssignments(map);
 
-		VisitExpression(&whileStmt->expr, map, false);
-		VisitStatement(&whileStmt->stmt, map, false);
-		VisitExpression(&whileStmt->expr, map, modifyAST);
-		VisitStatement(&whileStmt->stmt, map, modifyAST);
+		VisitExpression(&whileStmt->expr, map, false, modifyMap);
+		VisitStatement(&whileStmt->stmt, map, false, modifyMap);
 
 		MergeCopyAssignments(map, &beforeMap);
+
+		VisitExpression(&whileStmt->expr, map, modifyAST, false);
+		VisitStatement(&whileStmt->stmt, map, modifyAST, false);
 		break;
 	}
 	default: INVALID_VALUE(node->type);
@@ -308,8 +344,8 @@ void CopyPropagationPass(const AST* ast)
 			SectionStmt* section = node->ptr;
 			ASSERT(section->block.type == Node_BlockStatement);
 			if (section->sectionType != Section_Init)
-				VisitStatement(&section->block, &map, false);
-			VisitStatement(&section->block, &map, true);
+				VisitStatement(&section->block, &map, false, true);
+			VisitStatement(&section->block, &map, true, true);
 
 			FreeCopyAssignments(&map);
 		}
